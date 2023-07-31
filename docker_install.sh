dfbaa362436623d01a424c1011b8ca5b636a82bd
whr819987540 <819987540@qq.com>
2023-07-31 13:16:54 +0800

    安装最新docker的脚本

---
+++ docker_install.sh
@@ -0,0 +1,8 @@
+sudo apt update
+sudo apt install -y software-properties-common curl
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt install -y docker-ce docker-ce-cli containerd.io
+sudo systemctl enable docker
+sudo systemctl restart docker
+sudo systemctl status docker
