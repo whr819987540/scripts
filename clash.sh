6a410bafac273678d3834425059ee35cdd4096e8
whr819987540 <819987540@qq.com>
2023-07-31 13:16:22 +0800

    clash vpn的下载、配置、service脚本

---
+++ clash.sh
@@ -0,0 +1,50 @@
+# 下载
+config_url=""
+cd ~
+curl https://glados.rocks/tools/clash-linux.zip -o clash.zip
+unzip clash.zip
+cd clash
+curl $config_url > glados.yaml
+chmod a+x ./带版本号的可执行文件
+
+# 运行
+./clash-linux-amd64-v1.10.0 -f glados.yaml -d ./
+
+# 运行测试
+curl --proxy http://127.0.0.1:7890 https://www.google.com/
+pip install graphviz torchviz --proxy http://127.0.0.1:7890
+
+# 服务
+export HOME=~
+# sudo cat > filename <<EOF不行, 因为>也是操作符
+sudo sh -c "
+cat > /etc/systemd/system/clash.service <<EOF
+[Unit]
+Description=clash for windows
+
+[Service]
+Type=simple
+ExecStart=$HOME/clash/clash-linux-amd64-v1.10.0 -f $HOME/clash/glados.yaml -d $HOME/clash
+ExecReload=/home/whr-pc-ubuntu/下载/clash/clash-linux-amd64-v1.10.0 -f $HOME/clash/glados.yaml -d $HOME/clash
+RemainAfterExit=yes
+Restart=always
+RestartSec=60
+
+[Install]
+WantedBy=multi-user.target
+EOF
+"
+unset HOME
+# 重载配置文件
+sudo systemctl daemon-reload
+# 启动
+sudo systemctl start clash
+# 检查是否启动成功
+sudo systemctl status clash
+# 开机自启
+sudo systemctl enable clash
+
+# 环境变量
+echo "http_proxy=http://127.0.0.1:7890" >> ~/.bashrc
+source ~/.bashrc
+echo $http_proxy
