6f61deca76bbc7848617a8f5a4e4dd618bd321b8
whr819987540 <819987540@qq.com>
2023-07-31 13:19:06 +0800

    安装miniconda

---
+++ miniconda_install.sh
@@ -0,0 +1,4 @@
+# https://docs.conda.io/en/latest/miniconda.html#installing
+wget https://repo.anaconda.com/miniconda/Miniconda3-py38_23.5.2-0-Linux-x86_64.sh
+chmod a+x Miniconda3-py38_23.5.2-0-Linux-x86_64.sh
+./Miniconda3-py38_23.5.2-0-Linux-x86_64.sh
