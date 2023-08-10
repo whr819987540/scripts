f0b4ac83cd9bf33206714fb60669254d9f6753ce
whr819987540 <68956118+whr819987540@users.noreply.github.com>
2023-08-10 19:45:56 +0800

    add https_proxy in bashrc to clash.sh

--- clash.sh
+++ clash.sh
@@ -46,5 +46,6 @@ sudo systemctl enable clash
 
 # 环境变量
 echo "http_proxy=http://127.0.0.1:7890" >> ~/.bashrc
+echo "https_proxy=http://127.0.0.1:7890" >> ~/.bashrc
 source ~/.bashrc
 echo $http_proxy
