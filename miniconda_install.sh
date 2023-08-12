0f90c003edbb6f6909da6775c0d42ebb007cc1df
whr819987540 <68956118+whr819987540@users.noreply.github.com>
2023-08-12 15:10:40 +0800

    set http proxy for conda

--- miniconda_install.sh
+++ miniconda_install.sh
@@ -2,3 +2,8 @@
 wget https://repo.anaconda.com/miniconda/Miniconda3-py38_23.5.2-0-Linux-x86_64.sh
 chmod a+x Miniconda3-py38_23.5.2-0-Linux-x86_64.sh
 ./Miniconda3-py38_23.5.2-0-Linux-x86_64.sh
+conda init bash
+conda update -n base -c defaults conda
+conda config --show
+conda config --set proxy_servers.http $http_proxy
+conda config --set proxy_servers.https $http_proxy
