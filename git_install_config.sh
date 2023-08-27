9b92ab96dbe5553afc5b0c235d5c7f2e6ffd8319
whr819987540 <68956118+whr819987540@users.noreply.github.com>
2023-08-27 15:24:43 +0800

    use environment variable http_proxy

--- git_install_config.sh
+++ git_install_config.sh
@@ -4,7 +4,7 @@ sudo apt install -y git
 # 配置
 git config --global user.email "819987540@qq.com"
 git config --global user.name "whr81998754"
-git config --global http.proxy "http://127.0.0.1:7890/"
+git config --global http.proxy $http_proxy
 
 # github ssh login
 ssh-keygen
