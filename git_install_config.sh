0b48897da9dfe645eade3675e5c55b226a13d8ce
王浩然 <68956118+whr819987540@users.noreply.github.com>
2023-10-26 16:21:49 +0800

    use vscode as the default editor in GUI

--- git_install_config.sh
+++ git_install_config.sh
@@ -5,7 +5,10 @@ sudo apt install -y git
 git config --global user.email "819987540@qq.com"
 git config --global user.name "whr81998754"
 git config --global http.proxy $http_proxy
+# for command line 
 git config --global core.editor "/usr/bin/vim"
+# for GUI
+# git config --global core.editor "code --wait"
 
 # github ssh login
 ssh-keygen
